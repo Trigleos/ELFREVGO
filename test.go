9730d3b032c5700a7853cae4ded9d6f79f33800d
Trigleos <pol.thill@warwick.ac.uk>
2021-01-27 22:42:48 +0000

    Added loads of comments that can maybe make the functions clearer and added a bit of support for 32 bit however not finished yet. Added two major functions that implement the changes I was planning for

--- test.go
+++ test.go
@@ -5,18 +5,21 @@ import (
 	"io/ioutil"
 	"encoding/binary"
 	"strings"
+	"strconv"
 )
 
-type ELF struct {
+type ELF struct {  //ELF struct that is extensively used throughout this script
+	bit64 bool
 	section_table int
 	number_of_sections int
 	size_of_section_headers int
 	section_headers string
 	dynamic_strings string
+	string_table string
 }
 
 
-func read_file(filename string) (data []byte) {
+func read_file(filename string) (data []byte) {  //read in bytestream from file
 	data,err := ioutil.ReadFile(filename)
 	if err != nil {
 		fmt.Println("File reading error", err)
@@ -26,7 +29,7 @@ func read_file(filename string) (data []byte) {
 	return
 }
 
-func write_file(filename string, data []byte) error {
+func write_file(filename string, data []byte) error {  //write byte stream to a file
 	err := ioutil.WriteFile(filename,data,0777)
 	if err != nil {
 		fmt.Println("File writing error",err)
@@ -36,7 +39,7 @@ func write_file(filename string, data []byte) error {
 	return err
 }
 
-func change_class(data []byte) []byte {
+func change_class(data []byte) []byte {  //change class of file (64 bit or 32 bit) by switching a value in the ELF header
 	if data[4] == 2 {
 		data[4] = 1
 	} else {
@@ -45,7 +48,7 @@ func change_class(data []byte) []byte {
 	return data
 }
 
-func change_endianness(data []byte) []byte {
+func change_endianness(data []byte) []byte {  //change endianness of file by switching a value in the ELF header
 	if data[5] == 2 {
 		data[5] = 1
 	} else {
@@ -54,22 +57,57 @@ func change_endianness(data []byte) []byte {
 	return data
 }
 
-func read_sh_table(data []byte, curr_elf ELF) ELF {
+func write_data(hex_string string, data []byte, addr int, size int) []byte {  //write certain hex string to a specific address in little endian and return the changed byte stream
 
-	slice := data[62:64]
-	sh_table_header_addr := int(binary.LittleEndian.Uint16(slice)) * curr_elf.size_of_section_headers + curr_elf.section_table
+	for index := 0; index < size; index++ {
+		data[addr+index] = 0
+	}
 	
-	sh_table_header := data[sh_table_header_addr:sh_table_header_addr+curr_elf.size_of_section_headers]
+	hex_data := strings.Replace(hex_string, "0x", "", -1)
+	hex_data = strings.Replace(hex_data, "0X", "", -1) 
 	
-	slice = sh_table_header[24:32]
-	sh_table_addr := int(binary.LittleEndian.Uint64(slice))
+	length := len(hex_data)/2
+	for index := 0; index < length ; index++ {
+		current_int,err := strconv.ParseInt(hex_data[index*2:index*2+2],16,size*8)
+		if err == nil {
+			data[addr+(size-1-index)] = byte(current_int)
+		}
+	}
+	return data
 
+}
 
-	curr_strings := -1
+func read_sh_table(data []byte, curr_elf ELF) ELF {  //parses the section header string table that contains names of different sections
+
+	var sh_table_header_addr int
+	var sh_table_addr int
+	slice := data[0:1]
+	
+	if curr_elf.bit64 {
+		slice = data[62:64]  //This is the index of the section header string table in the section table
+		sh_table_header_addr = int(binary.LittleEndian.Uint16(slice)) * curr_elf.size_of_section_headers + curr_elf.section_table  //To get the address of the section header for the section header string table, mutliply the size of the headers by the index of the section header string table and add the initial offset at which the section header table starts
+	} else {  //Same process for 32 bit
+		slice = data[50:52]
+		sh_table_header_addr = int(binary.LittleEndian.Uint16(slice)) * curr_elf.size_of_section_headers + curr_elf.section_table
+	}
+		
+	sh_table_header := data[sh_table_header_addr:sh_table_header_addr+curr_elf.size_of_section_headers]  //gets the entire section header for the section header string table
+	
+	if curr_elf.bit64 {
+		slice = sh_table_header[24:32]  //gets the physical offset for the section header string table
+		sh_table_addr = int(binary.LittleEndian.Uint64(slice))
+	} else {  //same process for 32 bit
+		slice = sh_table_header[16:20]
+		sh_table_addr = int(binary.LittleEndian.Uint32(slice))
+	}
+
+	//the section header string table is a simple array of strings separated by Null bytes
+
+	curr_strings := -1  
 	index := sh_table_addr
 	var curr_byte int
 	
-	for curr_strings < curr_elf.number_of_sections {
+	for curr_strings < curr_elf.number_of_sections {  //scan section header string table until you have all section header names, sets up index as end of section header string table
 		curr_byte = int(data[index])
 		if curr_byte == 0 {
 			curr_strings++
@@ -77,117 +115,209 @@ func read_sh_table(data []byte, curr_elf ELF) ELF {
 		index++
 	}
 	
-	data_copy := data[sh_table_addr:index]
+	var data_copy []byte
+	data_copy = make([]byte,index-sh_table_addr)
+	copy(data_copy,data[sh_table_addr:index]) //copy the entire string table
 	
 	for index:=0; index < len(data_copy);index++ {
 		if data_copy[index] == 0 {
-			data_copy[index] = byte(' ')
+			data_copy[index] = byte(' ')	//replace null bytes by spaces
 		}
 	}
 	
-	curr_elf.section_headers = string(data_copy)
+	curr_elf.section_headers = string(data_copy) //add the whole thing as a single big string to the ELF struct
 	return curr_elf
 
 }
 
-func get_section_by_name(data []byte, curr_elf ELF, searched_section string) [4]int{
-	searched_index := strings.Index(curr_elf.section_headers,searched_section)
+func get_section_by_name(data []byte, curr_elf ELF, searched_section string) [4]int{ //uses section header string table and name of searched section to return address of a specific section
+	searched_index := strings.Index(curr_elf.section_headers,searched_section)  //In the section headers, sections refer to their names with an index into the section header string table
 	var result [4]int
 	
-	for index:=0;index<curr_elf.number_of_sections;index++ {
+	//function returns several key information on searched section:
+	//1: physical offset of section in the file
+	//2: size of the section in bytes
+	//3: the size in bytes of each entry, for sections that contain fixed-size entries, important for sections that are tables
+	//4: The virtual address of a section in memory, this is important to later convert virtual addresses to physical offsets
+	
+	for index:=0;index<curr_elf.number_of_sections;index++ {  //gets a whole section header using the initial offset of the section header table and the size for each section header
 		section_header := data[curr_elf.section_table +
 				index * curr_elf.size_of_section_headers:
 				curr_elf.section_table +
 				(index+1) * curr_elf.size_of_section_headers]
 				
-		string_offset := int(binary.LittleEndian.Uint32(section_header[0:4]))
+		string_offset := int(binary.LittleEndian.Uint32(section_header[0:4]))  //this refers to the name of a section, as mentioned above
 		if string_offset == searched_index {
-			result[0] = int(binary.LittleEndian.Uint64(section_header[24:32]))
-			result[1] = int(binary.LittleEndian.Uint64(section_header[32:40]))
-			result[2] = int(binary.LittleEndian.Uint64(section_header[56:64]))
-			result[3] = int(binary.LittleEndian.Uint64(section_header[16:24]))
+			if curr_elf.bit64 {
+				result[0] = int(binary.LittleEndian.Uint64(section_header[24:32]))  //gets physical offset
+				result[1] = int(binary.LittleEndian.Uint64(section_header[32:40]))  //gets size of section
+				result[2] = int(binary.LittleEndian.Uint64(section_header[56:64]))  //gets size of entry
+				result[3] = int(binary.LittleEndian.Uint64(section_header[16:24]))  //gets virtual address
+			} else {  //same process for 32 bits
+				result[0] = int(binary.LittleEndian.Uint32(section_header[16:20]))
+				result[1] = int(binary.LittleEndian.Uint32(section_header[20:24]))
+				result[2] = int(binary.LittleEndian.Uint32(section_header[36:40]))
+				result[3] = int(binary.LittleEndian.Uint32(section_header[12:16]))
+			}
 		}
 	}
 	
-	return result
+	return result  //returns the information on the section
 }
 
-func initialize_ELF(data []byte) ELF {
+func initialize_ELF(data []byte) ELF {  //initializes ELF struct by parsing ELF header
 	var curr_elf = ELF{}
+	curr_elf.bit64 = check_for_64bit(data)  //checks if ELF is 64 or 32 bit
 	
-	slice := data[40:48]
-	curr_elf.section_table = int(binary.LittleEndian.Uint64(slice))
+	if curr_elf.bit64 {
+		slice := data[40:48]
+		curr_elf.section_table = int(binary.LittleEndian.Uint64(slice))  //gets offset of section table
+		
+		slice = data[58:60]
+		curr_elf.size_of_section_headers = int(binary.LittleEndian.Uint16(slice)) //gets size of each section header in the section table
 	
-	slice = data[58:60]
-	curr_elf.size_of_section_headers = int(binary.LittleEndian.Uint16(slice))
+		slice = data[60:62]
+		curr_elf.number_of_sections = int(binary.LittleEndian.Uint16(slice))  //gets number of sections in the section table
+		
+	} else {
+		slice := data[32:36]
+		curr_elf.section_table = int(binary.LittleEndian.Uint32(slice))  //gets offset of section table
+		
+		slice = data[46:48]
+		curr_elf.size_of_section_headers = int(binary.LittleEndian.Uint16(slice))  //gets size of each section header in the section table
 	
-	slice = data[60:62]
-	curr_elf.number_of_sections = int(binary.LittleEndian.Uint16(slice))
+		slice = data[48:50]
+		curr_elf.number_of_sections = int(binary.LittleEndian.Uint16(slice))  //gets number of sections in the section table
+	}
 	
-	curr_elf = read_sh_table(data,curr_elf)
 	
-	curr_elf = read_dyn_str(data, curr_elf)
+	curr_elf = read_sh_table(data, curr_elf)  //parses section header string table that contains names of sections
+	
+	curr_elf = read_dyn_str(data, curr_elf)  //parses dynamic string table that contains names for dynamic functions
+	
+	curr_elf = read_str_tab(data, curr_elf) //parses string table that contains names for normal functions
 	
 	return curr_elf
 }
 
-func read_dyn_str(data []byte, curr_elf ELF) ELF{
-	dynstr := get_section_by_name(data,curr_elf, ".dynstr")
-	start_offset := dynstr[0]
-	length := dynstr[1]
+func read_dyn_str(data []byte, curr_elf ELF) ELF{  //reads the dynamic string table, a table that contains the names for dynamic functions such as external library functions
+	dynstr := get_section_by_name(data,curr_elf, ".dynstr")  //this is where the names are stored
+	start_offset := dynstr[0] //start of the section
+	length := dynstr[1] //size of the section
 	
-	data_copy := data[start_offset:start_offset+length]
+	var data_copy []byte
+	data_copy = make([]byte,length)
+	copy(data_copy,data[start_offset:start_offset+length])  //takes a copy from the whole section
 	
-	for index:=0; index < len(data_copy);index++ {
+	//the dynstr section is basically a long array containing zero-terminated strings that represent the names for the dynamic symbols
+	
+	for index:=0; index < len(data_copy);index++ {  //loop over the section and replace null bytes with spaces
 		if data_copy[index] == 0 {
 			data_copy[index] = byte(' ')
 		}
 	}
 	
-	curr_elf.dynamic_strings = string(data_copy)
+	curr_elf.dynamic_strings = string(data_copy)  //convert the whole section into one big string and save it in the ELF struct
 	return curr_elf
 }
 
-func get_dyn_function_id_by_name(data []byte, curr_elf ELF, searched_string string) int{
-	searched_index := strings.Index(curr_elf.dynamic_strings,searched_string)
+func read_str_tab(data []byte, curr_elf ELF) ELF{  //reads the string table, a table that contains the names for every symbol such as function names
+	strtab := get_section_by_name(data,curr_elf, ".strtab")  //this is where the names are stored
+	if strtab[0] != 0 {
+		start_offset := strtab[0] //start of the section
+		length := strtab[1] //size of the section
+		
+		var data_copy []byte
+		data_copy = make([]byte,length)
+		copy(data_copy,data[start_offset:start_offset+length])  //takes a copy from the whole section
+		
+		//the strtab section is basically a long array containing zero-terminated strings that represent the names for the symbols
+		
+		for index:=0; index < len(data_copy);index++ {  //loop over the section and replace null bytes with spaces
+			if data_copy[index] == 0 {
+				data_copy[index] = byte(' ')
+			}
+		}
+		
+		curr_elf.string_table = string(data_copy)  //convert the whole section into one big string and save it in the ELF struct
+		return curr_elf
+	} else {
+		fmt.Println("This executable seems to be stripped.\nCannot find string table\n")
+		return curr_elf
+	}
+}
+
+func get_dyn_function_id_by_name(data []byte, curr_elf ELF, searched_string string) int{  //returns ID for dynamic function. This ID is used internally by the runtime linker to resolve dynamic function addresses and we need it to get the GOT address
+	searched_index := strings.Index(curr_elf.dynamic_strings,searched_string)  //The dynsym table again refers to the name of each dynamic symbol using an offset into the dynstr table which we parsed earlier
 	
-	dynsym := get_section_by_name(data, curr_elf, ".dynsym")
-	dynsym_section := data[dynsym[0]:dynsym[0]+dynsym[1]]
+	dynsym := get_section_by_name(data, curr_elf, ".dynsym") //dynsym is the table that contains all the dynamic symbols
+	dynsym_section := data[dynsym[0]:dynsym[0]+dynsym[1]]  //get the entire section
 	
-	for index := 0; index<dynsym[1]/dynsym[2]; index++{
-		str_index := int(binary.LittleEndian.Uint32(dynsym_section[index*dynsym[2]:index*dynsym[2]+4]))
+	for index := 0; index<dynsym[1]/dynsym[2]; index++{  //we can easily loop over this section because we know the size for each entry in the dynsym table
+		str_index := int(binary.LittleEndian.Uint32(dynsym_section[index*dynsym[2]:index*dynsym[2]+4]))  //This is an index into the dynstr table
 		if str_index == searched_index {
-			return index
+			return index  //the dynamic symbols are simply numbered from top to bottom so we can simply return the index
 		}
 		
 	}
 	return 0
 }
 
-func get_dyn_addr_by_name(data []byte, curr_elf ELF, searched_function string) int{
-	id := get_dyn_function_id_by_name(data,curr_elf,searched_function)
-	rela_plt := get_section_by_name(data,curr_elf,".rela.plt")
-	rela_plt_section := data[rela_plt[0]:rela_plt[0]+rela_plt[1]]
-	for index:=0;index < rela_plt[1]/rela_plt[2];index++ {
-		plt_entry := rela_plt_section[index*rela_plt[2]:(index+1)*rela_plt[2]]
-		info := int(binary.LittleEndian.Uint64(plt_entry[8:16])) >> 32
+func get_dyn_addr_by_name(data []byte, curr_elf ELF, searched_function string) int{  //returns the virtual address for a dynamic function
+	id := get_dyn_function_id_by_name(data,curr_elf,searched_function)  //gets the ID for the specific function we're searching for
+	rela_plt := get_section_by_name(data,curr_elf,".rela.plt")  //The rela.plt section contains the relocation information that is used by the runtime linker to determine where he needs to put the addresses for the dynamic library functions. More specifically it contains addresses in the GOT, which we want to overwrite.
+	rela_plt_section := data[rela_plt[0]:rela_plt[0]+rela_plt[1]] //get entire section
+	for index:=0;index < rela_plt[1]/rela_plt[2];index++ {  //loop over this section 
+		plt_entry := rela_plt_section[index*rela_plt[2]:(index+1)*rela_plt[2]]  //get one entry
+		info := int(binary.LittleEndian.Uint64(plt_entry[8:16])) >> 32  //info is the second field in each rela.plt entry. It links each entry to its name and contains other information. To get the function ID, we need to shift the value to the left by 32
 		if info == id {
-			return int(binary.LittleEndian.Uint64(plt_entry[0:8]))
+			return int(binary.LittleEndian.Uint64(plt_entry[0:8]))  //This is the virtual address at which the linker will save the address for the searched function. This address is in the GOT, more specifically in the plt part of the GOT
 		}
 	}
 
 	return 0
 }
 
-func vir_got_addr_to_phys_addr(data []byte, curr_elf ELF, vir_addr int) int{
-	got_plt := get_section_by_name(data, curr_elf, ".got.plt")
-	offset := vir_addr - got_plt[3]
-	phys_addr := got_plt[0] + offset
+func vir_addr_to_phys_addr(data []byte, curr_elf ELF, vir_addr int, section_name string) int{  //translates virtual address to physical address based on section in which this address is
+	section_info := get_section_by_name(data, curr_elf,section_name)  //get section information
+	offset := vir_addr - section_info[3]  //subtracts the start address of the virtual section from the virtual address to get the offset into the section
+	phys_addr := section_info[0] + offset  //adds this offset to the physical start of the section to get physical offset
 	return phys_addr
 }
 
-func check_for_pie(data []byte) bool{
-	if data[16] == 2 {
+func check_for_pie(data []byte) bool{  //checks if ELF file is PIE (position independent executable). If yes, there's no point in replacing entries in the GOT with predefined values
+	if data[16] == 2 {  //This is the type field of the ELF header that specifies the type of an ELF file. 2 means EXEC which means that the ELF is not PIE. If it is, the value is 3 which stands for DYN
+		return true
+	} else {
+		return false
+	}
+}
+
+func check_for_64bit(data []byte) bool{  //parses ELF header to determine wether ELF is 64 bit or not
+	if data[4] == 2 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func overwrite_got_entry(data []byte, function_name string, new_function_address string, curr_elf ELF) ([]byte) {
+	addr := get_dyn_addr_by_name(data, curr_elf, function_name)
+	addr = vir_addr_to_phys_addr(data, curr_elf, addr, ".got.plt")
+	data = write_data(new_function_address,data,addr,4)
+	return data
+}
+
+func overwrite_section_header_types(data []byte, curr_elf ELF) ([]byte) {
+	for index := 0; index < curr_elf.number_of_sections; index++ {
+		addr := curr_elf.section_table + index*curr_elf.size_of_section_headers + 4
+		data = write_data("",data,addr,4)
+	}
+	return data
+}
+
+func check_stripped(data []byte, curr_elf ELF) {
+	if (get_Section_by_name(data,curr_elf,".symtab")[0] == 0 {
 		return true
 	} else {
 		return false
@@ -196,11 +326,13 @@ func check_for_pie(data []byte) bool{
 
 func main() {
 	
-	data := read_file("nopie")
+	data := read_file("nopie_stripped")
 	curr_elf := initialize_ELF(data)
+	
+	data = overwrite_got_entry(data, "system" , "0x00401176",curr_elf)
+	
+	data = overwrite_section_header_types(data, curr_elf)
+	
+	write_file("nopie_go",data)
 
-	addr := get_dyn_addr_by_name(data, curr_elf, "system")
-	fmt.Printf("%x\n",addr)
-	addr = vir_got_addr_to_phys_addr(data, curr_elf, addr)
-	fmt.Printf("%x\n",addr)
 }
